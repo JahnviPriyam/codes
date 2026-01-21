8dcf318c44f8617aecff655e790dfb086c3c3b4b
JahnviPriyam <jahnvi27oct@gmail.com>
2026-01-21 09:54:45 +0530

    Time: 1 ms (100%), Space: 43.4 MB (52.75%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxPower(String s) {
+        int n= s.length();
+        int max=1; int count=1;
+        for(int i=1; i<n; i++)
+        {
+            if(s.charAt(i)==s.charAt(i-1))
+            {
+                count++;
+            }
+            else
+            {
+                count=1;
+            }
+            if(count>max)
+            {
+                max=count;
+            }
+        }
+        return max;
+        
+    }
+}

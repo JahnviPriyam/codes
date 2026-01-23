3c3d98f89bfde018a3fe2740a262196ded975c5e
JahnviPriyam <jahnvi27oct@gmail.com>
2026-01-23 09:57:17 +0530

    Time: 1 ms (99.55%), Space: 74 MB (29.51%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,29 +1,19 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        int i = 0;
-
-        while (i < n) {
-            if (nums[i] > 0 && nums[i] <= n) {
-                int correctIndex = nums[i] - 1;
-
-                if (nums[i] != nums[correctIndex]) {
-                    // Swap nums[i] with nums[correctIndex]
-                    int temp = nums[i];
-                    nums[i] = nums[correctIndex];
-                    nums[correctIndex] = temp;
-                    continue; // stay on the same i to check new value
-                }
-            }
-            i++;
-        }
-
-        for (i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
+        int n=nums.length;
+        int[] freq= new int[n+1];
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]>0 && nums[i]<=n){
+            freq[nums[i]]++;
             }
         }
-
-        return n + 1;
+        for(int i=1; i<=n; i++)
+        {
+            if(freq[i]==0)
+            return i;
+        }
+        return n+1;
+        
     }
 }

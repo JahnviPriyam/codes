6c395885481906af9c2ac82e1e981454876e5c3c
JahnviPriyam <jahnvi27oct@gmail.com>
2026-01-23 11:05:26 +0530

    Time: 8 ms (10.75%), Space: 46.2 MB (24.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,9 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s= String.valueOf(x);
-        StringBuilder sb= new StringBuilder(s);
-        String rev= sb.reverse().toString();
-        int num= Integer.parseInt(s);
-        return s.equals(rev);
+       
+       String s= String.valueOf(x);
+       StringBuilder sb= new StringBuilder(s);
+       String rev= sb.reverse().toString();
+       if(s.equals(rev))
+       {
+        return true;
+       }
+       else
+       {
+        return false;
+       }
+        
     }
 }

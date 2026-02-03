175d559afe583277c7b02489cff43cfbd2339b68
JahnviPriyam <jahnvi27oct@gmail.com>
2026-02-03 12:38:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Deletion at the end of a Linked List/deletion-at-the-end-of-a-linked-list.java
@@ -0,0 +1,30 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
+*/
+
+class Solution {
+    public Node removeLastNode(Node head) {
+        // code here
+        Node temp= head;
+        if(head==null||head.next==null)
+        {
+            return null;
+        }
+        while(temp.next.next!=null)
+        {
+            temp= temp.next;
+        }
+        temp.next=null;
+        return head;
+    }
+}
